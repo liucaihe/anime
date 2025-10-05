8fac68d8d9ae371120280c09a51789ad81931b3a
Julian Garnier <julian.garnier@gmail.com>
2025-10-05 15:15:40 +0200

    Use container instead of target to calculate scale, otherwise calling . clientHeight on the target will return null on SVG elements

--- src/events/scroll.js
+++ src/events/scroll.js
@@ -137,6 +137,8 @@ class ScrollContainer {
     /** @type {Number} */
     this.top = 0;
     /** @type {Number} */
+    this.scale = 1;
+    /** @type {Number} */
     this.zIndex = 0;
     /** @type {Number} */
     this.scrollX = 0;
@@ -248,6 +250,7 @@ class ScrollContainer {
       height = $el.clientHeight;
       this.top = elRect.top;
       this.left = elRect.left;
+      this.scale = elRect.width ? width / elRect.width : (elRect.height ? height / elRect.height : 1);
     }
     this.width = width;
     this.height = height;
@@ -773,10 +776,9 @@ export class ScrollObserver {
       }
     }
     const rect = $target.getBoundingClientRect();
-    const offsetUnscaled = isHori ? rect.left + container.scrollX - container.left : rect.top + container.scrollY - container.top;
-    const scale = rect.width ? $target.clientWidth / rect.width : (rect.height ? $target.clientHeight / rect.height : 1);
-    const offset = offsetUnscaled * scale;
-    const targetSize = isHori ? rect.width * scale : rect.height * scale;
+    const scale = container.scale;
+    const offset = (isHori ? rect.left + container.scrollX - container.left : rect.top + container.scrollY - container.top) * scale;
+    const targetSize = (isHori ? rect.width : rect.height) * scale;
     const containerSize = isHori ? container.width : container.height;
     const scrollSize = isHori ? container.scrollWidth : container.scrollHeight;
     const maxScroll = scrollSize - containerSize;
--- tests/playground/sandbox/index.js
+++ tests/playground/sandbox/index.js
@@ -1,46 +1,14 @@
-import { waapi, animate, $, set, stagger, spring, random, utils } from '../../../dist/modules/index.js';
+import { waapi, animate, onScroll, $, set, stagger, spring, random, utils } from '../../../dist/modules/index.js';
 
-// const targets = $('.green');
-
-// const [$red] = $('.red');
-
-// set(targets, { y: () => random(-200, 200), x: () => random(-200, 200) });
-
-// set(targets, { backgroundColor: '#FF0000' });
-
-// let reverse = false;
-
-// const animation = animate(targets, {
-//   x: 'var(--x)',
-//   y: 'var(--y)',
-//   backgroundColor: 'var(--test-color, #FF00FF)',
-//   borderRadius: '40%',
-//   rotate: 90,
-//   scale: 1.25,
-//   delay: stagger(100),
-//   loop: true,
-//   alternate: true,
-//   onComplete: () => {
-//     console.log('completed');
-//   }
-// });
-
-// document.body.addEventListener('click', () => {
-//   animation.revert();
-// });
-
-// Set the CSS variables as properties on the animated elements
-utils.set('.square', {
-  '--radius': '4px',
-  '--x': '0rem',
-  '--pseudo-el-after-scale': '1', // applied to the pseudo element "::after"
-  borderRadius: 'var(--radius)',
-  translateX: 'var(--x)',
-});
-
-// Animate the values of the CSS variables
-animate('.square', {
-  '--radius': '20px',
-  '--x': '16.5rem',
-  '--pseudo-el-after-scale': '1.55' // Animates the ":after" pseudo element of the element
+const container = document.querySelector("main");
+animate(".rectangle", {
+  x: "70px",
+  ease: "inOutCirc",
+  delay: 1000,
+  loopDelay: 1000,
+  loop: true,
+  autoplay: onScroll({
+    container,
+    debug: true
+  })
 });
